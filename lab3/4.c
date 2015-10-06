0b18d8043b4febad1f783edae9b1cedaa9ce7004
Harsh Shah <harsh.shah.jayesh@gmail.com>
2015-10-06 13:48:30 +0530

    Fixed 4.c

--- lab3/4.c
+++ lab3/4.c
@@ -6,6 +6,9 @@
 
 void reverse(char s[], int length);
 void copy(int ifd, int ofd);
+void strcat_modified(char one[], char two[], int length);
+
+char all_characters[100000] = "";
 
 int main(int argc, char *argv[])
 {
@@ -29,19 +32,36 @@ void copy(int ifd, int ofd)
 	char buffer[512];
 	ssize_t c;
 	while ((c = read(ifd, buffer, sizeof(buffer))) != 0) {
-		reverse(buffer, c);
-		write(ofd, buffer, c);
+		strcat_modified(all_characters, buffer, c);	
 	}
+	reverse(all_characters, strlen(all_characters));
+	write(ofd, all_characters, strlen(all_characters));
 }
 
 void reverse(char s[], int length) 
 {
 	int i;
-	char reverse[length];
-	for (i = 0; i < length; ++i) {
-		reverse[i] = s[length - i - 1];
-	}
-	for (i = 0; i < length; ++i) {
-		s[i] = reverse[i];
+	int j;
+	i = 0;
+	j = length - 1;
+	while (i < j) {
+		char temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
 	}
 }
+
+void strcat_modified(char one[], char two[], int length) 
+{
+	int i = strlen(one);
+	int j = 0;
+	while (j < length) {
+		one[i] = two[j];
+		i++;
+		j++;
+	}
+	one[i] = '\0';
+}
+
